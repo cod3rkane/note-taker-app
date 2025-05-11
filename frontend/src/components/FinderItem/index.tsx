5c80d78d141faf02f8ea8b6b4e3d75caf9b5778f
Cod3rkane <lopes.coder@gmail.com>
2025-05-11 13:55:07 -0400

    adds mouse event handler for FinderItem - Canceling user right mouse click

--- frontend/src/components/FinderItem/index.tsx
+++ frontend/src/components/FinderItem/index.tsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import classNames from 'classnames'
 import type { FinderItemProps } from './types'
 import styles from './styles.module.scss'
 
 export function FinderItem(props: FinderItemProps) {
 	const [isHidden, setIsHidden] = useState(false)
+	const ref = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		const handleContextMenu = (event: MouseEvent) => {
+			if (
+				event.target === ref.current ||
+				['ASIDE', 'aside'].includes((event.target as HTMLDivElement)?.nodeName)
+			) {
+				event.preventDefault()
+			}
+		}
+
+		document.addEventListener('contextmenu', handleContextMenu)
+
+		return () => {
+			document.removeEventListener('contextmenu', handleContextMenu)
+		}
+	}, [])
+
 	const icon = props.isFolder ? '📂' : '📄'
 	const onClick = () => {
 		if (props.isFolder) {
@@ -16,6 +35,7 @@ export function FinderItem(props: FinderItemProps) {
 
 	return (
 		<div
+			ref={ref}
 			className={classNames(styles.finderItem, props.className, {
 				[styles.hidden]: isHidden,
 			})}
